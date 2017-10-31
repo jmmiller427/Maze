b5a83fba0a3e58da47d65730be9e389521611623
jmmiller427 <jmmi234@g.uky.edu>
2017-10-31 12:00:34 -0400

    Deleted comments from Cell class

--- src/Cell.java
+++ src/Cell.java
@@ -2,15 +2,6 @@
  * Date: 23 October 2017
  * Class: CS 335 - 001
  * Project: Maze
- *
- * Structure: - Create a class for each individual Cell. The Cells will change based on how many rows and columns
- *              there are.
- *            - Each cell will also have a visited flag on it.
- *            - Each cell needs 4 lines to draw the walls around it. A line goes on top, bottom, left and right.
- *            - The lines will have an array of booleans, so the wall will be drawn if it is true, and not drawn if
- *              it is false.
- *            - Each cell will have an i and j value, to represent where it is on the grid. Each i, j value will
- *              represent the top left corner of the cell and lines (walls) are drawn from here
  */
 
 import javax.swing.*;
@@ -19,7 +10,7 @@ import java.awt.*;
 
 class Cell extends JPanel{
 
-    private boolean visited = false;
+//    private boolean visited = false;
     int[] walls = {1, 1, 1, 1};
 
     int i, j;
